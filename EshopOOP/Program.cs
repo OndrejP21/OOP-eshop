f600b9692dd5b29f9aa79fcd863635e432d7eb1d
OndrejP21 <ondrapetera@seznam.cz>
2026-04-08 09:56:46 +0200

    Init

--- EshopOOP/Program.cs
+++ EshopOOP/Program.cs
@@ -1,10 +1,11 @@
-﻿namespace EshopOOP
+﻿using EshopOOP.utils;
+
+namespace EshopOOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
         }
     }
 }
